87aa2d938973d3122457eda1e4c5a34e2dbcd6ae
Arno Haase <arno.haase@haase-consulting.com>
2021-05-03 09:26:54 +0200

    string exercise

---
+++ src/main/java/com/ajjpj/javaworkshop/primitives/String_Exercise1.java
@@ -0,0 +1,15 @@
+package com.ajjpj.javaworkshop.primitives;
+
+
+public class String_Exercise1 {
+
+
+
+    public int numWhitespaces(String s) {
+        return 0; //TODO
+    }
+
+    public String firstIdentifier(String s) {
+        return ""; //TODO
+    }
+}
