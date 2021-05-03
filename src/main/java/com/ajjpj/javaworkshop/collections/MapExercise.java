bf09507fc6b4d41d17f280098779eb866ff2baea
Arno Haase <arno.haase@haase-consulting.com>
2021-05-03 14:36:32 +0200

    map exercise

---
+++ src/main/java/com/ajjpj/javaworkshop/collections/MapExercise.java
@@ -0,0 +1,25 @@
+package com.ajjpj.javaworkshop.collections;
+
+import java.util.Set;
+
+public class MapExercise {
+}
+
+class TeilnehmerVerwaltung {
+
+
+    // *nie* null liefern, sondern ggf. leere Liste
+    Set<String> teilnehmerFuerKurs(String kurs) {
+        return Set.of(); //TODO
+    }
+
+    // Kurs wird ggf. automatisch angelegt
+    void registriereTeilnehmer(String kurs, String teilnehmer) {
+        //TODO
+    }
+
+    // optional
+    void entferneTeilnehmer(String kurs, String teilnehmer) {
+        //TODO
+    }
+}
