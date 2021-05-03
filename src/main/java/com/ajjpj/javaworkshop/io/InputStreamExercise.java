70dcf011bc55d217c2b2802d7afc5f546cbec8b2
Arno Haase <arno.haase@haase-consulting.com>
2021-05-03 10:31:51 +0200

    input streams exercise

--- src/main/java/com/ajjpj/javaworkshop/io/InputStreamExercise.java
+++ src/main/java/com/ajjpj/javaworkshop/io/InputStreamExercise.java
@@ -1,13 +1,32 @@
 package com.ajjpj.javaworkshop.io;
 
-import java.io.File;
+import java.io.*;
 
 public class InputStreamExercise {
-    public static int count99(File f) {
-        return 0; //TODO
+    public static int count99(File f) throws IOException {
+        int count = 0;
+        try(InputStream in = new FileInputStream(f)) {
+            int b;
+            while((b = in.read()) != -1) {
+                if(b == 99) count += 1;
+            }
+        }
+        return count;
     }
 
-    public static void separateEvenUneven(File input, File outputEven, File outputUneven) {
-        //TODO
+    public static void separateEvenOdd(File input, File outputEven, File outputOdd) throws IOException {
+        try(InputStream in = new FileInputStream(input);
+                OutputStream outEven = new FileOutputStream(outputEven);
+                OutputStream outOdd = new FileOutputStream(outputOdd)) {
+            int b;
+            while((b = in.read()) != -1) {
+                if ((b & 1) == 0) {
+                    outEven.write(b);
+                }
+                else {
+                    outOdd.write(b);
+                }
+            }
+        }
     }
 }
