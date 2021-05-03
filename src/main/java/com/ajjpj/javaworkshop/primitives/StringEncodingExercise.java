e4d96e4035f18766e1d3e6006c447de30859c3b3
Arno Haase <arno.haase@haase-consulting.com>
2021-05-03 10:03:18 +0200

    string encoding exercise

--- src/main/java/com/ajjpj/javaworkshop/primitives/StringEncodingExercise.java
+++ src/main/java/com/ajjpj/javaworkshop/primitives/StringEncodingExercise.java
@@ -1,8 +1,10 @@
 package com.ajjpj.javaworkshop.primitives;
 
+import java.nio.charset.StandardCharsets;
 
 public class StringEncodingExercise {
     public static double bytesPerCharacterUtf8(String s) {
-        return 1; //TODO
+        byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+        return bytes.length * 1.0 / s.length();
     }
 }
